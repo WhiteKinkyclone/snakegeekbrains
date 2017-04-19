82e951e4371c0530ddf6d5d08667f7c7601635d9
Kinky Good <Kinky Good>
2017-04-19 18:27:37 +0500

    Write functions

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,9 +10,25 @@ namespace snake
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("HW!");
+			int x1 = 1;
+			int y1 = 3;
+			char sym1 = '*';
+
+			Draw(x1, y1, sym1);			
+
 			Console.ReadLine();
 
+			int x2 = 3;
+			int y2 = 5;
+			char sym2 = '#';
+
+			Draw(x2, y2, sym2);
+
+		}
+		static void Draw(int x, int y, char sym)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);
 		}
 	}
 }
